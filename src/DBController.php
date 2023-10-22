4c3a5eeae74ad9dd7e2937150b600fd50722f381
carry0987 <carry0987@users.noreply.github.com>
2023-10-23 01:01:04 +0800

    Replace MySQLi with PDO

--- src/DBController.php
+++ src/DBController.php
@@ -1,6 +1,8 @@
 <?php
 namespace carry0987\RememberMe;
 
+use PDO;
+
 class DBController
 {
     private $connectDB = null;
@@ -8,14 +10,8 @@ class DBController
     public function connectDB(string $host, string $user, string $password, string $database, int $port = 3306)
     {
         try {
-            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-            //Handle Exception of MySQLi
-            $driver = new \mysqli_driver();
-            $driver->report_mode = MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT;
-            $this->connectDB = new \mysqli($host, $user, $password, $database, $port);
-            $this->mysqli_version = $this->connectDB->server_info;
-            $this->connectDB->set_charset('utf8mb4');
-        } catch (\mysqli_sql_exception $e) {
+            $this->connectDB = new PDO("mysql:host=$host;dbname=$database;port=$port;charset=utf8mb4", $user, $password);
+        } catch (\PDOException $e) {
             echo $this->throwDBError($e->getMessage(), $e->getCode());
             exit();
         }
@@ -35,20 +31,12 @@ class DBController
     public function getUserByName(string $username)
     {
         $results = array();
-        $query = 'SELECT uid, password FROM user WHERE username = ?';
-        $stmt = $this->connectDB->stmt_init();
+        $query = $this->connectDB->prepare('SELECT uid, password FROM user WHERE username = ?');
         try {
-            $stmt->prepare($query);
-            $stmt->bind_param('s', $username);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            if ($result->num_rows != 0) {
-                while ($row = $result->fetch_assoc()) {
-                    $results[] = $row;
-                }
-            }
-            return isset($results[0]['uid']) ? $results[0] : false;
-        } catch (\mysqli_sql_exception $e) {
+            $query->execute([$username]);
+            $results = $query->fetchAll(PDO::FETCH_ASSOC);
+            return (!empty($results)) ? $results : false;
+        } catch (\PDOException $e) {
             echo self::throwDBError($e->getMessage(), $e->getCode());
             return false;
         }
@@ -56,24 +44,14 @@ class DBController
 
     public function getTokenByUserID(int $userID, string $selector)
     {
-        $query = 'SELECT pw_hash, expiry_date, user.username FROM remember_me 
+        $query = $this->connectDB->prepare('SELECT pw_hash, expiry_date, user.username FROM remember_me 
                 INNER JOIN user ON user.uid = remember_me.user_id 
-                WHERE user_id = ? AND selector_hash = ?';
-        $stmt = $this->connectDB->stmt_init();
+                WHERE user_id = ? AND selector_hash = ?');
         try {
-            $stmt->prepare($query);
-            $stmt->bind_param('is', $userID, $selector);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            if ($result->num_rows != 0) {
-                while ($row = $result->fetch_assoc()) {
-                    $results = $row;
-                }
-            } else {
-                $results = false;
-            }
-            return $results;
-        } catch (\mysqli_sql_exception $e) {
+            $query->execute([$userID, $selector]);
+            $results = $query->fetch(PDO::FETCH_ASSOC);
+            return (!empty($results)) ? $results : false;
+        } catch (\PDOException $e) {
             echo self::throwDBError($e->getMessage(), $e->getCode());
             return false;
         }
@@ -81,16 +59,13 @@ class DBController
 
     public function resetToken(string $selector)
     {
-        $query = 'UPDATE remember_me SET pw_hash = ?, expiry_date = ? WHERE selector_hash = ?';
-        $stmt = $this->connectDB->stmt_init();
+        $query = $this->connectDB->prepare('UPDATE remember_me SET pw_hash = ?, expiry_date = ? WHERE selector_hash = ?');
         $empty = '';
         $date = 0;
         try {
-            $stmt->prepare($query);
-            $stmt->bind_param('sis', $empty, $date, $selector);
-            $stmt->execute();
+            $query->execute([$empty, $date, $selector]);
             return true;
-        } catch (\mysqli_sql_exception $e) {
+        } catch (\PDOException $e) {
             echo self::throwDBError($e->getMessage(), $e->getCode());
             return false;
         }
@@ -98,15 +73,12 @@ class DBController
 
     public function updateToken(int $userID, string $selector, string $pw_hash)
     {
-        $query = 'UPDATE remember_me SET pw_hash = ?, expiry_date = ? WHERE user_id = ? AND selector_hash = ?';
-        $stmt = $this->connectDB->stmt_init();
+        $query = $this->connectDB->prepare('UPDATE remember_me SET pw_hash = ?, expiry_date = ? WHERE user_id = ? AND selector_hash = ?');
         $getTime = time() + (30 * 24 * 60 * 60);
         try {
-            $stmt->prepare($query);
-            $stmt->bind_param('siis', $pw_hash, $getTime, $userID, $selector);
-            $stmt->execute();
+            $query->execute([$pw_hash, $getTime, $userID, $selector]);
             return true;
-        } catch (\mysqli_sql_exception $e) {
+        } catch (\PDOException $e) {
             echo self::throwDBError($e->getMessage(), $e->getCode());
             return false;
         }
@@ -114,20 +86,16 @@ class DBController
 
     public function insertToken(int $userID, string $selector, string $random_pw_hash, int $expiry_date = 0)
     {
-        $query = 'INSERT INTO remember_me (user_id, selector_hash, pw_hash, expiry_date) VALUES (?, ?, ?, ?)';
-        $stmt = $this->connectDB->stmt_init();
+        $query = $this->connectDB->prepare('INSERT INTO remember_me (user_id, selector_hash, pw_hash, expiry_date) VALUES (?, ?, ?, ?)');
         try {
-            $stmt->prepare($query);
-            $stmt->bind_param('issi', $userID, $selector, $random_pw_hash, $expiry_date);
-            $stmt->execute();
+            $query->execute([$userID, $selector, $random_pw_hash, $expiry_date]);
             return true;
-        } catch (\mysqli_sql_exception $e) {
+        } catch (\PDOException $e) {
             echo self::throwDBError($e->getMessage(), $e->getCode());
             return false;
         }
     }
 
-    //Throw database error excetpion
     private static function throwDBError(string $message, int $code)
     {
         $error = '<h1>Service unavailable</h1>'."\n";
